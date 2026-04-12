c286f1fd7fb2c17ef33eaa2825c350a75f11eb59
Aurumleif <alobadiesel@gmail.com>
2026-04-12 19:05:31 +0800

    Add back to top button with SVG icon

--- script.js
+++ script.js
@@ -52,3 +52,20 @@ function handleScrollEffects() {
 window.addEventListener("load", handleScrollEffects);
 window.addEventListener("scroll", handleScrollEffects);
 window.addEventListener("resize", handleScrollEffects);
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
